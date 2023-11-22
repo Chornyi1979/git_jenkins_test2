20f042642ee69665dff7e28da2ff2f0acc9d61e4
jenkins <jenkins@example.com>
2023-11-22 15:03:31 +0200

    war21

---
+++ src/main/java/com/example/HelloWorldServletTest.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HelloWorldServletTest {
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private RequestDispatcher requestDispatcher;
+
+    @Test
+    public void testDoGet() throws ServletException, IOException {
+        // Arrange
+        when(request.getRequestDispatcher("/index.jsp")).thenReturn(requestDispatcher);
+
+        // Act
+        new HelloWorldServlet().doGet(request, response);
+
+        // Assert
+        verify(request).setAttribute("message", "Hello, World!");
+        verify(requestDispatcher).forward(request, response);
+    }
+}
